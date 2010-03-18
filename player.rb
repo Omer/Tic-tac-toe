1ad0d237ac7390e40b9abfba1594071112b3adc6
Omer Jakobinsky <s0831911@sms.ed.ac.uk>
2010-03-18 17:30:33 +0000

    added find_next method to Player.

--- player.rb
+++ player.rb
@@ -10,6 +10,10 @@ class Player
     "Name: " + @name + ", Symbol: " + @symbol
   end
   
+  def self.find_next
+    ObjectSpace.each_object(Player).cycle.next
+  end
+  
   def self.find_by_symbol(symbol)
     found = nil
     ObjectSpace.each_object(Player) { |player| found = player if player.symbol == symbol }
